fc7c2c5790912cc69d64ce0a45a02d70deebef6a
Rocky Tseng <rocky.tseng@kla-tencor.com>
2024-01-03 22:11:55 -0800

    Update

--- checkDevice.cu
+++ checkDevice.cu
@@ -89,6 +89,8 @@ int main(int argc, char **argv)
 
        CHECK(cudaSetDevice(dev));
 
+
+      // https://docs.nvidia.com/cuda/cuda-runtime-api/structcudaDeviceProp.html
        cudaDeviceProp deviceProp;
 
        CHECK(cudaGetDeviceProperties(&deviceProp, dev));
@@ -97,66 +99,77 @@ int main(int argc, char **argv)
 
        cudaDriverGetVersion(&driverVersion);
        cudaRuntimeGetVersion(&runtimeVersion);
-       printf("  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n",
+       printf("  CUDA Driver Version / Runtime Version                      %d.%d / %d.%d\n",
               driverVersion / 1000, (driverVersion % 100) / 10,
               runtimeVersion / 1000, (runtimeVersion % 100) / 10);
-       printf("  CUDA Capability Major/Minor version number:    %d.%d\n",
+       printf("  CUDA Capability Major/Minor version number:                %d.%d\n",
               deviceProp.major, deviceProp.minor);
-       printf("  Total amount of global memory:                 %.2f GBytes (%llu "
-              "bytes)\n", (float)deviceProp.totalGlobalMem / pow(1024.0, 3),
+       printf("  Total amount of global memory:                             %.2f GBytes (%llu bytes)\n",
+              (float)deviceProp.totalGlobalMem / pow(1024.0, 3),
               (unsigned long long)deviceProp.totalGlobalMem);
-       printf("  GPU Clock rate:                                %.0f MHz (%0.2f "
-              "GHz)\n", deviceProp.clockRate * 1e-3f,
+       printf("  GPU Clock rate:                                            %.0f MHz (%0.2f GHz)\n",
+              deviceProp.clockRate * 1e-3f,
               deviceProp.clockRate * 1e-6f);
-       printf("  Memory Clock rate:                             %.0f Mhz\n",
+       printf("  Memory Clock rate:                                         %.0f Mhz\n",
               deviceProp.memoryClockRate * 1e-3f);
-       printf("  Global memory Bus Width:                       %d-bit\n",
+       printf("  Global memory Bus Width:                                   %d-bit\n",
               deviceProp.memoryBusWidth);
 
        if (deviceProp.l2CacheSize)
        {
-           printf("  L2 Cache Size:                                 %d bytes\n",
+           printf("  L2 Cache Size:                                             %d bytes\n",
                   deviceProp.l2CacheSize);
        }
 
-       printf("  Max Texture Dimension Size (x,y,z)             1D=(%d), "
-              "2D=(%d,%d), 3D=(%d,%d,%d)\n", deviceProp.maxTexture1D,
+       printf("  Max Texture Dimension Size (x,y,z)                         1D=(%d), 2D=(%d,%d), 3D=(%d,%d,%d)\n",
+              deviceProp.maxTexture1D,
               deviceProp.maxTexture2D[0], deviceProp.maxTexture2D[1],
               deviceProp.maxTexture3D[0], deviceProp.maxTexture3D[1],
               deviceProp.maxTexture3D[2]);
-       printf("  Max Layered Texture Size (dim) x layers        1D=(%d) x %d, "
-              "2D=(%d,%d) x %d\n", deviceProp.maxTexture1DLayered[0],
+       printf("  Max Layered Texture Size (dim) x layers                    1D=(%d) x %d, 2D=(%d,%d) x %d\n",
+              deviceProp.maxTexture1DLayered[0],
               deviceProp.maxTexture1DLayered[1], deviceProp.maxTexture2DLayered[0],
               deviceProp.maxTexture2DLayered[1],
               deviceProp.maxTexture2DLayered[2]);
-       printf("  Total amount of constant memory:               %lu bytes\n",
+       printf("  Total amount of constant memory:                           %lu bytes\n",
               deviceProp.totalConstMem);
-       printf("  Total amount of shared memory per block:       %lu bytes\n",
+       printf("  Total amount of shared memory per block:                   %lu bytes\n",
               deviceProp.sharedMemPerBlock);
-       printf("  Total number of registers available per block: %d\n",
+       printf("  Total number of registers available per block:             %d\n",
               deviceProp.regsPerBlock);
-       printf("  Warp size:                                     %d\n",
+       printf("  Warp size:                                                 %d\n",
               deviceProp.warpSize);
-       printf("  Maximum number of threads per multiprocessor:  %d\n",
+       printf("  Maximum number of threads per multiprocessor:              %d\n",
               deviceProp.maxThreadsPerMultiProcessor);
-       printf("  Maximum number of threads per block:           %d\n",
+       printf("  Maximum number of threads per block:                       %d\n",
               deviceProp.maxThreadsPerBlock);
-       printf("  Maximum sizes of each dimension of a block:    %d x %d x %d\n",
+       printf("  Maximum sizes of each dimension of a block:                %d x %d x %d\n",
               deviceProp.maxThreadsDim[0],
               deviceProp.maxThreadsDim[1],
               deviceProp.maxThreadsDim[2]);
-       printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d\n",
+       printf("  Maximum sizes of each dimension of a grid:                 %d x %d x %d\n",
               deviceProp.maxGridSize[0],
               deviceProp.maxGridSize[1],
               deviceProp.maxGridSize[2]);
-       printf("  Maximum memory pitch:                          %lu bytes\n",
+       printf("  Maximum memory pitch:                                      %lu bytes\n",
               deviceProp.memPitch);
-       printf("  Number of multiprocessor:                      %d\n",
+       printf("  Number of multiprocessor:                                  %d\n",
               deviceProp.multiProcessorCount);
-       printf("  Number of CUDA cores per device:               %d\n",
+       printf("  Number of CUDA cores per device:                           %d\n",
               getSPcores(deviceProp) );
-       printf("  Number of CUDA cores per multiprocessor:       %d\n",
+       printf("  Number of CUDA cores per multiprocessor:                   %d\n",
               getSPcores(deviceProp)/deviceProp.multiProcessorCount );
+       printf("  Number of 32-bit registers per multiprocessor:             %d\n",
+              deviceProp.regsPerMultiprocessor );
+       printf("  Number of 32-bit registers per block:                      %d\n",
+              deviceProp.regsPerBlock );
+       printf("  Number of asynchronous engines:                            %d\n",
+              deviceProp.asyncEngineCount);
+       printf("  Device can possibly execute multiple kernels concurrently: %d\n", deviceProp.concurrentKernels);
+       //printf(": %d\n", deviceProp.)
+       //printf(": %d\n", deviceProp.)
+       //printf(": %d\n", deviceProp.)
+       //printf(": %d\n",)
     }
 
     exit(EXIT_SUCCESS);
